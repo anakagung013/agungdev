04cffd9ddba8a72613036ae4f83fa5f0f6770dec
Anak Agung Putu Gede Jaya Nugraha <anakagungutugedejayanugraha@gmail.com>
2024-03-05 17:55:00 -0800

    Initial commit

---
+++ components/Testimoni.js
@@ -0,0 +1,184 @@
+import React, { useState } from "react";
+
+// import react slick
+import Slider from "react-slick";
+import Image from "next/image";
+import Stars from "../public/assets/Icon/stars.svg";
+import ArrowBack from "../public/assets/Icon/eva_arrow-back-fill.svg";
+import ArrowNext from "../public/assets/Icon/eva_arrow-next-fill.svg";
+
+const Testimoni = ({
+  listTestimoni = [
+    {
+      name: "Ammar Raihan",
+      city: "Denpasar",
+      country: "Indonesia",
+      rating: "5",
+      testimoni:
+        "Wow... I am very happy to use this Agung Music, it turned out to be more than my expectations and so far there have been no problems. Agung Music always the best",
+    },
+    {
+      name: "Tami",
+      city: "Denpasar",
+      country: "Indonesia",
+      rating: "4.5",
+      testimoni:
+        "I Have no word, it's very very gooooood....",
+    },
+    {
+      name: "Unknown",
+      city: "Unknown",
+      country: "Unknown",
+      rating: "4.5",
+      testimoni:
+        "GOOOOOOODDDDDDDD",
+    },
+  ],
+}) => {
+  const settings = {
+    dots: true,
+    customPaging: function (i) {
+      return (
+        <a className="">
+          <span className="mx-2 rounded-l-full rounded-r-full h-4 w-4 block cursor-pointer transition-all "></span>
+        </a>
+      );
+    },
+    dotsClass: "slick-dots w-max absolute mt-20  ",
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 770,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  const [sliderRef, setSliderRef] = useState(null);
+
+  return (
+    <>
+      <Slider
+        {...settings}
+        arrows={false}
+        ref={setSliderRef}
+        className="flex items-stretch justify-items-stretch"
+      >
+        {listTestimoni.map((listTestimonis, index) => (
+          <div className="px-3 flex items-stretch" key={index}>
+            <div className="border-2 border-gray-500 hover:border-orange-500 transition-all rounded-lg p-8 flex flex-col">
+              <div className="flex flex-col xl:flex-row w-full items-stretch xl:items-center">
+                <div className="flex order-2 xl:order-1">
+                  <Image
+                    src={listTestimonis.image}
+                    height={50}
+                    width={50}
+                    alt="Icon People"
+                  />
+                  <div className="flex flex-col ml-5 text-left">
+                    <p className="text-lg text-black-600 capitalize">
+                      {listTestimonis.name}
+                    </p>
+                    <p className="text-sm text-black-500 capitalize">
+                      {listTestimonis.city},{listTestimonis.country}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-none items-center ml-auto order-1 xl:order-2">
+                  <p className="text-sm">{listTestimonis.rating}</p>
+                  <span className="flex ml-4">
+                    <Stars className="h-4 w-4" />
+                  </span>
+                </div>
+              </div>
+              <p className="mt-5 text-left">“{listTestimonis.testimoni}”.</p>
+            </div>
+          </div>
+        ))}
+      </Slider>
+      <div className="flex w-full items-center justify-end">
+        <div className="flex flex-none justify-between w-auto mt-14">
+          <div
+            className="mx-4 flex items-center justify-center h-14 w-14 rounded-full bg-white border-orange-500 border hover:bg-orange-500 hover:text-white-500 transition-all text-orange-500 cursor-pointer"
+            onClick={sliderRef?.slickPrev}
+          >
+            <ArrowBack className="h-6 w-6 " />
+          </div>
+          <div
+            className="flex items-center justify-center h-14 w-14 rounded-full bg-white border-orange-500 border hover:bg-orange-500 hover:text-white-500 transition-all text-orange-500 cursor-pointer"
+            onClick={sliderRef?.slickNext}
+          >
+            <ArrowNext className="h-6 w-6" />
+          </div>
+        </div>
+      </div>
+      
+      <form action="https://formspree.io/f/xayropbz" method="POST" className="container">
+      <h1>Get in touch</h1>
+      <div className="email block">
+        <label htmlFor="frm-email">Email</label>
+        <input
+          id="frm-email"
+          type="email"
+          name="email"
+          autoComplete="email"
+          required
+        />
+      </div>
+      <div className="block phone">
+        <label htmlFor="frm-phone">Phone</label>
+        <input
+          id="frm-phone"
+          type="text"
+          name="phone"
+          autoComplete="tel"
+          required
+        />
+      </div>
+      <div className="name block">
+        <div>
+          <label htmlFor="frm-first">First Name</label>
+          <input
+            id="frm-first"
+            type="text"
+            name="first"
+            autoComplete="given-name"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="frm-last">Last Name</label>
+          <input
+            id="frm-last"
+            type="text"
+            name="last"
+            autoComplete="family-name"
+            required
+          />
+        </div>
+      </div>
+      <div className="message block">
+        <label htmlFor="frm-message">Message</label>
+        <textarea id="frm-message" rows="6" name="message"></textarea>
+      </div>
+      <div className="button block">
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+    </>
+  );
+};
+export default Testimoni;
